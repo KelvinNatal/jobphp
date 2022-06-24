c2207e251e8c2fb07831c3404b0fdbddca76e6b2
KelvinNatal <kelvinnatal@hotmail.com>
2022-06-24 19:48:29 -0300

    Updating Polymorphism

--- Classes/book.php
+++ Classes/book.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'products.php';
-
 class Book extends Products {       
 
     protected $weight;
--- Classes/dvd.php
+++ Classes/dvd.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once 'products.php';
-
-
 class Dvd extends Products {       
 
     protected $size;
